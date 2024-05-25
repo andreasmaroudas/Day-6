c9b0aee803e368700121294ed9a4f1302292c56f
user <user@DESKTOP-C7T5PGB>
2024-05-25 14:46:18 +0300

    Add project files.

---
+++ ArenaGameEngine/Necromancer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class Necromancer : Hero
+    {
+        public bool Imune { get; set; }
+        public Necromancer() : base("Necromancer")
+        {
+        }
+        public Necromancer(string name) : base(name)
+        {
+        }
+        public override void TakeDamage(int incomingDamage)
+        {
+            int n = Random.Shared.Next(0, 100);
+            if (n <= 50 || Imune)
+            {
+                incomingDamage = 0;
+            }
+            base.TakeDamage(incomingDamage);
+        }
+
+        public override int Attack()
+        {
+            int baseAttack = base.Attack();
+            int n = Random.Shared.Next(0, 100);
+            if (n <= 10)
+            {
+                baseAttack = Strength * 4;
+            }
+            return baseAttack;
+        }
+    }
+}
---
+++ ArenaGameEngine/Wizard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class Wizard : Hero
+    {
+        public Wizard() : base("Wizard")
+        {
+        }
+        public Wizard(string name) : base(name)
+        {
+        }
+
+        
+
+        public void Heal()
+        {
+            if (Mana >= 15)
+            {
+                Console.WriteLine($"{Name} casts a healing spell.");
+                Health += 20; 
+                Mana -= 15; 
+            }
+            else
+            {
+                Console.WriteLine($"{Name} does not have enough mana to heal.");
+            }
+        }
+        public void CastSpell(string spell)
+        {
+            if (Mana >= 10)
+            {
+                Console.WriteLine($"{Name} casts {spell}!");
+                Mana -= 10; 
+            }
+            else
+            {
+                Console.WriteLine($"{Name} does not have enough mana to cast {spell}.");
+            }
+        }
+
+
+        
+    }
+}
